381908549fbd016f59ad3b1b1bae595d911082bd
North <1227379879@qq.com>
2022-10-11 12:12:13 +0800

    fix: 修复输出格式

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,14 @@ export const jsonToInterface = (json: any, interfaceName = 'IRoot'): string => {
 					}
 				}
 
+				// 替换result 中values的\ 和 “
+				for (const key in result) {
+					if (Object.prototype.hasOwnProperty.call(result, key)) {
+						const value = result[key]
+						result[key] = value.replaceAll('\\', '').replaceAll('"', '')
+					}
+				}
+
 				dataType = `${interfaceStr && interfaceStr}
 			     \n interface ${interfaceName} ${JSON.stringify(result, null, 4)}`
 				break
